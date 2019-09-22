f80721ff7f2ee561fbb72e066182012c3f649dee
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-09-23 02:01:40 +0530

    Deleted every things + test text

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,5 +1,20 @@
-<template></template>
+<template>
+  <v-app>
+    <p class="display-4 font-weight-black text-center">Test Text</p>
+  </v-app>
+</template>
 
 <script>
 export default {};
 </script>
+
+
+<style >
+body {
+  background-image: linear-gradient(
+    109.6deg,
+    rgba(193, 98, 253, 1) 11.2%,
+    rgba(0, 49, 165, 1) 91.1%
+  );
+}
+</style>
