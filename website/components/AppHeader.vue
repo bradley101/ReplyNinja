b74024a29608c4ff4a77d08f5a7d4255f6b113a7
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-09-24 00:11:02 +0530

    +Header Components

---
+++ website/components/AppHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-toolbar
+    :extended="extended"
+    :prominent="prominent"
+    :dense="dense"
+    :collapse="collapse"
+    :flat="flat"
+    :src="bg ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : undefined"
+    :extension-height="extensionHeight"
+  >
+    <template v-if="extendedSlot" #extension>
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+    </template>
+
+    <v-toolbar-title>Ninja Reply</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <v-toolbar-items>
+      <v-btn text>Link 1</v-btn>
+      <v-btn text>Link 2</v-btn>
+      <v-btn text>Link 3</v-btn>
+    </v-toolbar-items>
+
+    <template v-if="$vuetify.breakpoint.smAndUp">
+      <v-btn icon>
+        <v-icon>mdi-export-variant</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-delete-circle</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-plus-circle</v-icon>
+      </v-btn>
+    </template>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    extended: false,
+    extendedSlot: false,
+    prominent: false,
+    dense: false,
+    collapse: false,
+    flat: false,
+    bg: false,
+    extensionHeight: 48
+  })
+};
+</script>
--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -1,18 +1,64 @@
+@@ -0,0 +1,117 @@
 <template>
-  <div>
-    <header />
-    <nuxt />
-  </div>
+  <v-app dark>
+    <app-header />
+    <!-- <v-toolbar
+      :extended="extended"
+      :prominent="prominent"
+      :dense="dense"
+      :collapse="collapse"
+      :flat="flat"
+      :src="bg ? 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg' : undefined"
+      :extension-height="extensionHeight"
+    >
+      <template v-if="extendedSlot" #extension>
+        <v-toolbar-items>
+          <v-btn text>Link 1</v-btn>
+          <v-btn text>Link 2</v-btn>
+          <v-btn text>Link 3</v-btn>
+        </v-toolbar-items>
+      </template>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>-->
+
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import header from "~/components/header";
+import AppHeader from "~/components/AppHeader";
 export default {
   components: {
-    header
+    AppHeader
   }
 };
 </script>
-
-<style >
-</style>
