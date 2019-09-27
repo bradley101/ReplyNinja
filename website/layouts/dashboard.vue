54faf9b44620458b0325a906578f78dd2174d852
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-09-27 21:21:56 +0530

    +dasboard initial layout

---
+++ website/layouts/dashboard.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app dark>
+    <v-card height="100vh">
+      <v-navigation-drawer absolute permanent left>
+        <template v-slot:prepend>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+
+            <v-list-item link two-line>
+              <v-list-item-content>
+                <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+                <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-icon>mdi-menu-down</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item v-for="item in items" :key="item.title" @click>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </v-app>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" }
+      ]
+    };
+  }
+};
+</script>
