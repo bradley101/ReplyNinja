5a217cccb6f4f2e41a20fc944fe00979cadd52c5
Saurabh Chandra Rai <35683286+SAURABHRAI110@users.noreply.github.com>
2019-09-27 13:44:47 +0530

    +styling

--- website/pages/signin.vue
+++ website/pages/signin.vue
@@ -6,28 +6,34 @@
     </div>
 
     <div class="login-right">
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title class="pb-0">
-          <h1>Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <img class="login-rn-logo" src="~assets/reply-ninja-logo.svg" alt="reply ninja logo" />
+      <p class="font-weight-light title text-left home-hero-tagline">
+        Welcome, please login to
+        <br />your account
+      </p>
+
+      <div class="social-login">
+        <span class="google-login"></span>
+        <span class="github-login"></span>
+      </div>
+
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
     </div>
   </div>
 </template>
